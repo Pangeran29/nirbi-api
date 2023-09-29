17fc13b221dfd5a1437e52cc97a56c8708ba8b04
pangeran.jonathan <jonathan@maxsol.id>
2023-09-29 23:09:42 +0700

    feat: configuration set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.string().required(),
+        DATABASE_URL: Joi.string().required(),
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
