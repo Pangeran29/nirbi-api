b58affe63365422d70fe9a6a8f102c20f41a7ec7
pangeran.jonathan <jonathan@maxsol.id>
2023-10-27 09:37:21 +0700

    fix: error import

--- libs/helper/src/helper.module.ts
+++ libs/helper/src/helper.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { HelperService } from './helper.service';
 import { FileSystemModule } from './file-system/file-system.module';
 
 @Module({
-  providers: [HelperService],
-  exports: [HelperService],
   imports: [FileSystemModule],
 })
 export class HelperModule {}
